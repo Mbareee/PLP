2a14d0bd56815d8e969960d72f3f79bbb2b0ca7f
Mbareee <mbariwilson23@gmail.com>
2024-03-18 19:44:46 +0300

    age

---
+++ Dart/variablehack.dart
@@ -0,0 +1,9 @@
+void main(){
+
+var name = "Mbaree";
+var age = 27;
+var school = "University of Nairobi";
+var hobby = "Playing video games";
+
+print("My name is $name and I am $age years old. I go to $school and my hobby is $hobby.");
+}
